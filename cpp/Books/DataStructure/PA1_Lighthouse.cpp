69b273b22c21e02e582fddd38d0d6f4f88426843
jatom <wenjun.huang80@gmail.com>
2024-04-15 01:08:28 +0800

    做数据结构上pa

--- cpp/Books/DataStructure/PA1_Lighthouse.cpp
+++ cpp/Books/DataStructure/PA1_Lighthouse.cpp
@@ -12,13 +12,16 @@ using Pos = pair<int64_t, int64_t>;
 constexpr int N = 4000000;
 Pos lightHouses[N];
 int n;
-int count = 0;
-
+int64_t count = 0;
 
 void merge(int p, int q, int r) {
     auto x = q - p + 1;
     auto y = r - q;
 
+    if (lightHouses[q].second <= lightHouses[q + 1].second) {
+        count += x * y;
+        return;
+    }
     auto b = new Pos[x];
     auto c = new Pos[y];
 
@@ -27,38 +30,33 @@ void merge(int p, int q, int r) {
 
     auto i = 0;
     auto j = 0;
-    auto k = p;
-    while (i < x && j < y) {
-        if (b[i].second <= c[j].second) {
-            lightHouses[k] = b[i++];
-            count += (y - j);
-        } else {
+    for (auto k = p; k <= r; k++) {
+        if (i == x) {
             lightHouses[k] = c[j++];
+        } else if (j == y) {
+            lightHouses[k] = b[i++];
+        } else if (b[i].second > c[j].second) {
+            lightHouses[k] = c[j++];
+        } else {
+            lightHouses[k] = b[i++];
+            count += y - j;
         }
-        k++;
     }
+
     delete[] c;
     delete[] b;
 }
 
 void mergeSort(int p, int r) {
     if (p < r) {
-        auto mid = (p + r) / 2;
+        auto mid = p + (r - p) / 2;
         mergeSort(p, mid);
         mergeSort(mid + 1, r);
         merge(p, mid, r);
     }
 }
 
-int main() {
-    scanf("%d", &n);
-    for (auto i = 0; i < n; i++) {
-        int x, y;
-        scanf("%d %d", &x, &y);
-        lightHouses[i].first = x;
-        lightHouses[i].second = y;
-    }
-
+void go() {
     qsort(lightHouses, n, sizeof(Pos), [](const void *x, const void *y) {
         auto a = (Pos *) x;
         auto b = (Pos *) y;
@@ -67,5 +65,29 @@ int main() {
 
     mergeSort(0, n - 1);
     cout << count << endl;
+}
+
+void test() {
+    lightHouses[0] = {2, 2};
+    lightHouses[1] = {4, 3};
+    lightHouses[2] = {5, 1};
+    n = 3;
+    go();
+}
+
+void judge() {
+    scanf("%d", &n);
+    for (auto i = 0; i < n; i++) {
+        int x, y;
+        scanf("%d %d\n", &x, &y);
+        lightHouses[i].first = x;
+        lightHouses[i].second = y;
+    }
+    go();
+}
+
+int main() {
+//    test();
+    judge();
 
 }
