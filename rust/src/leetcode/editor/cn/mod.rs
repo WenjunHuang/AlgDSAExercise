7ecbf33d236fc3dddbc3f2b1d37046820481fd1e
jatom-百年酒馆 <wenjun.huang80@gmail.com>
2024-04-28 16:21:27 +0800

    滑动窗口

--- rust/src/leetcode/editor/cn/mod.rs
+++ rust/src/leetcode/editor/cn/mod.rs
@@ -4,6 +4,6 @@ mod minimum_depth_of_binary_tree;
 mod util;
 pub mod minimum_window_substring;
 pub mod longest_substring_without_repeating_characters;
-pub mod RemoveDuplicatesFromSortedListIi;
 pub mod FindPivotIndex;
 pub mod ProductOfArrayExceptSelf;
+pub mod MinimumWindowSubstring;
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,9 @@
 mod leetcode;
-use self::leetcode::editor::cn::RemoveDuplicatesFromSortedListIi::Solution;
+use self::leetcode::editor::cn::MinimumWindowSubstring::Solution;
 
 fn main() {
+    let s = "ADOBECODEBANC".to_string();
+    let t = "ABC".to_string();
+    let res = Solution::min_window(s, t);
+    println!("{}", res);
 }
