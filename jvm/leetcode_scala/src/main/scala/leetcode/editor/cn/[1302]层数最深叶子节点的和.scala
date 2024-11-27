d09bd9d9a57ce293666a5d0bf632779b3d42957e
jatom-百年酒馆 <wenjun.huang80@gmail.com>
2024-11-27 16:21:09 +0800

    刷题

--- jvm/leetcode_scala/src/main/scala/leetcode/editor/cn/[1302]层数最深叶子节点的和.scala
+++ jvm/leetcode_scala/src/main/scala/leetcode/editor/cn/[1302]层数最深叶子节点的和.scala
@@ -1,24 +1,51 @@
 package leetcode.editor.cn
 
-object DeepestLeavesSum  {
+object DeepestLeavesSum {
 
-  //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
- *   var value: Int = _value
- *   var left: TreeNode = _left
- *   var right: TreeNode = _right
- * }
- */
-object Solution {
-    def deepestLeavesSum(root: TreeNode): Int = {
-        
-    }
-}
+  // leetcode submit region begin(Prohibit modification and deletion)
+  /** Definition for a binary tree node. class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) { var value: Int = _value var left: TreeNode = _left var right: TreeNode =
+    * _right }
+    */
+  object Solution {
+    def deepestLeavesSum(root: TreeNode): Int = dfs(root)
+
+    import scala.collection.mutable
+    private def bfs(root: TreeNode): Int =
+      root match
+        case null => 0
+        case _ =>
+          val q   = mutable.Queue[TreeNode](root)
+          var res = 0
+          while q.nonEmpty do
+            res = (0 until q.size).foldLeft(0) { (accum, _) =>
+              val cur = q.dequeue()
+              if cur.left != null then q.enqueue(cur.left)
+              if cur.right != null then q.enqueue(cur.right)
+              accum + cur.value
+            }
+
+          res
+
+    private def dfs(root: TreeNode): Int =
+      root match
+        case null => 0
+        case _ =>
+          val res = mutable.ArrayBuffer[Int]()
+          def traverse(node: TreeNode, depth: Int): Unit =
+            node match
+              case null => ()
+              case _ =>
+                if res.size == depth then res.addOne(node.value)
+                else res(depth) = res(depth) + node.value
+
+                traverse(node.left, depth + 1)
+                traverse(node.right, depth + 1)
+
+          traverse(root,0)
+          res.last
+  }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  
   import scala.util.control.TailCalls.*
 
   /** 栈安全的State Monad。可用于leetcode的用fp方式来解决问题。
